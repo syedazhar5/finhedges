bafff273d171e43c7cceff2d577fe2de63c391ef
Syed Azhar <syed.azhar9465@gmail.com>
2022-05-17 17:04:10 +0530

    images added

--- services.php
+++ services.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-3">
             <div class="card">
-                <img class="card-img-top img-fluid" src="images\unlisted.jpg" alt="Card image cap">
+                <img class="card-img-top img-fluid" src="images\abc.jpg" alt="Card image cap">
                 <div class="card-body p-3">
                     <blockquote class="blockquote mb-0">
                         <h4 class="card-title mt-0">UNLISTED SHARES </h4>
@@ -24,12 +24,16 @@
                     <h1 class="display-6"><b>Unlisted Shares<b></h1>
                     <p class="lead">An unlisted security is a financial instrument that is not traded on a formal
                         exchange because it does not meet listing requirements. Unlisted securities are also called OTC
-                        securities, as trading is done on the over-the-counter(OTC) market mostly by market makers. Unlisted stocks can be tracked via pink sheets or on the
-                        OTCBB. Unlisted securities are usually issued by smaller or new firms that cannot or do not wish
-                        to comply with the requirements of an official exchange, such as market capitalization
-                        thresholds or listing fees. Furthermore, because they are not exchange traded, unlisted
-                        securities are often less liquid than listed securities. Unlisted stock can be tracked via pink
-                        sheets or on the Over-The-Counter Bulletin Board (OTCBB).
+                        securities, as trading is done on the over-the-counter(OTC) market mostly by market makers.
+                        Unlisted stocks can be tracked via pink sheets or on the OTCBB. Unlisted securities are usually
+                        issued by smaller or new firms that cannot or do not wish to comply with the requirements of an
+                        official exchange, such as market capitalization thresholds or listing fees. Furthermore,
+                        because they are not exchange traded, unlisted securities are often less liquid than listed
+                        securities. Unlisted stock can be tracked via pink sheets or on the Over-The-Counter Bulletin
+                        Board (OTCBB). A hedge fund is a pooled investment fund that trades in relatively liquid assets
+                        and is able to make extensive use of more complex trading, portfolio-construction, and risk
+                        management techniques in an attempt to improve performance, such as short selling, leverage, and
+                        derivatives.
                     </p>
                 </div>
             </div>
@@ -42,7 +46,7 @@
                 <div class="card-body p-3">
                     <blockquote class="blockquote mb-0">
                         <h4 class="card-title mt-0">ALGO TRADING </h4>
-                        <p class="card-text text-muted font-size-13">An unlisted security is a financial instrument that
+                        <p class="card-text ">An unlisted security is a financial instrument that
                             is not traded on a formal exchange because it does not meet listing requirements.
                         </p>
                     </blockquote>
@@ -53,11 +57,17 @@
             <div class="jumbotron m-2">
                 <div class="container">
                     <h1 class="display-6"><b>Algo Trading<b></h1>
-                    <p class="lead">An unlisted security is a financial instrument that is not traded on a formal
-                        exchange because it does not meet listing requirements. Unlisted securities are also called OTC
-                        securities, as trading is done on the over-the-counter
-                        (OTC) market mostly by market makers. Unlisted stocks can be tracked via pink sheets or on the
-                        OTCBB.
+                    <p class="lead">Algorithmic trading is a method of executing orders using automated pre-programmed
+                        trading instructions accounting for variables such as time, price, and volume. This type of
+                        trading attempts to leverage the speed and computational resources of computers relative to
+                        human traders. In the twenty-first century, algorithmic trading has been gaining traction with
+                        both retail and institutional traders. It is widely used by investment banks, pension
+                        funds, mutual funds, and hedge funds that may need to spread out the execution of a larger order
+                        or perform trades too fast for human traders to react to. A study in 2019 showed that around 92%
+                        of trading in the Forex market was performed by trading algorithms rather than humans. The term
+                        algorithmic trading is often used synonymously with automated trading system. These encompass a
+                        variety of trading strategies, some of which are based on formulas and results from mathematical
+                        finance, and often rely on specialized software.
                     </p>
                 </div>
             </div>
@@ -70,9 +80,8 @@
                 <div class="card-body p-3">
                     <blockquote class="blockquote mb-0">
                         <h4 class="card-title mt-0">HEDGING SERVICES </h4>
-                        <p class="card-text text-muted font-size-13">What Is a Hedge?
-                            A hedge is an investment that is made with the intention of reducing the risk of adverse
-                            price movements in an asset
+                        <p class="card-text">A hedge fund is an investment partnership—the marriage of a fund manager
+                            and investors who pool their money together into the fund.
                         </p>
                     </blockquote>
                 </div>
@@ -82,11 +91,50 @@
             <div class="jumbotron m-2">
                 <div class="container">
                     <h1 class="display-6"><b>Hedging Services<b></h1>
-                    <p class="lead">An unlisted security is a financial instrument that is not traded on a formal
-                        exchange because it does not meet listing requirements.
-                        Unlisted securities are also called OTC securities, as trading is done on the over-the-counter
-                        (OTC) market mostly by market makers.
-                        Unlisted stocks can be tracked via pink sheets or on the OTCBB.
+                    <p class="lead">A hedge fund is a pooled investment fund that trades in relatively liquid assets and
+                        is able to make extensive use of more complex trading, portfolio-construction, and risk
+                        management techniques in an attempt to improve performance, such as short selling, leverage, and
+                        derivatives.[1] Financial regulators generally restrict hedge fund marketing to institutional
+                        investors, high net worth individuals, and others who are considered sufficiently sophisticated.
+                        Hedge funds are considered alternative investments. Their ability to use leverage and more
+                        complex investment techniques distinguishes them from regulated investment funds available to
+                        the retail market, commonly known as mutual funds and ETFs. They are also considered distinct
+                        from private equity funds and other similar closed-end funds as hedge funds generally invest in
+                        relatively liquid assets and are usually open-ended.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 mt-5">
+            <div class="card">
+                <img class="card-img-top img-fluid" src="images\pms.jpg" alt="Card image cap">
+                <div class="card-body p-3">
+                    <blockquote class="blockquote mb-0">
+                        <h4 class="card-title mt-0">Portfolio Management Services (PMS) </h4>
+                        <p class="card-text">Portfolio Management Services (PMS) is a facility offered by a portfolio
+                            manager with the intent to achieve the required rate of return.
+                        </p>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col-9">
+            <div class="jumbotron m-2">
+                <div class="container">
+                    <h1 class="display-6"><b>Portfolio Management Services(PMS)<b></h1>
+                    <p class="lead">A hedge fund is a pooled investment fund that trades in relatively liquid assets and
+                        is able to make extensive use of more complex trading, portfolio-construction, and risk
+                        management techniques in an attempt to improve performance, such as short selling, leverage, and
+                        derivatives.[1] Financial regulators generally restrict hedge fund marketing to institutional
+                        investors, high net worth individuals, and others who are considered sufficiently sophisticated.
+                        Hedge funds are considered alternative investments. Their ability to use leverage and more
+                        complex investment techniques distinguishes them from regulated investment funds available to
+                        the retail market, commonly known as mutual funds and ETFs. They are also considered distinct
+                        from private equity funds and other similar closed-end funds as hedge funds generally invest in
+                        relatively liquid assets and are usually open-ended. This means they typically allow investors
+                        to invest and withdraw capital periodically based on the fund's net asset value, whereas
+                        private-equity funds generally invest in illiquid assets and only return capital after a number
+                        of years.
                     </p>
                 </div>
             </div>
--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="col-lg-12">
-        <p style="background: #20201E;" class="text-center text-white p-2">
+        <p style="background: #20201E;" class="text-center text-white">
             © Copyright 2022 - All Rights Reserved.<br>
         </p>
     </div>
