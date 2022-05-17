1d5c16673bece995cd37637f45421c61b3d3c94e
Syed Azhar <syed.azhar9465@gmail.com>
2022-05-17 13:02:57 +0530

    services images and information added

--- services.php
+++ services.php
@@ -5,7 +5,7 @@
 <br>
 <div class="container">
     <div class="row">
-        <div class="col-xl-3">
+        <div class="col-3">
             <div class="card">
                 <img class="card-img-top img-fluid" src="images\unlisted.jpg" alt="Card image cap">
                 <div class="card-body p-3">
@@ -18,27 +18,55 @@
                 </div>
             </div>
         </div>
+        <div class="col-9">
+            <div class="jumbotron m-2">
+                <div class="container">
+                    <h1 class="display-6"><b>Unlisted Shares<b></h1>
+                    <p class="lead">An unlisted security is a financial instrument that is not traded on a formal
+                        exchange because it does not meet listing requirements. Unlisted securities are also called OTC
+                        securities, as trading is done on the over-the-counter(OTC) market mostly by market makers. Unlisted stocks can be tracked via pink sheets or on the
+                        OTCBB. Unlisted securities are usually issued by smaller or new firms that cannot or do not wish
+                        to comply with the requirements of an official exchange, such as market capitalization
+                        thresholds or listing fees. Furthermore, because they are not exchange traded, unlisted
+                        securities are often less liquid than listed securities. Unlisted stock can be tracked via pink
+                        sheets or on the Over-The-Counter Bulletin Board (OTCBB).
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="row mt-5">
-        <div class="col-xl-3">
+        <div class="col-lg-3">
             <div class="card">
                 <img class="card-img-top img-fluid" src="images\AlgoTrading.jpg" alt="Card image cap">
                 <div class="card-body p-3">
                     <blockquote class="blockquote mb-0">
-                        <h4 class="card-title mt-0">ALGO TRADING</h4>
-                        <p class="card-text text-muted font-size-13">Algorithmic trading is a method of executing orders
-                            using automated pre-programmed trading instructions accounting for variables such as time,
-                            price, and volume.
+                        <h4 class="card-title mt-0">ALGO TRADING </h4>
+                        <p class="card-text text-muted font-size-13">An unlisted security is a financial instrument that
+                            is not traded on a formal exchange because it does not meet listing requirements.
                         </p>
                     </blockquote>
                 </div>
             </div>
         </div>
+        <div class="col-9">
+            <div class="jumbotron m-2">
+                <div class="container">
+                    <h1 class="display-6"><b>Algo Trading<b></h1>
+                    <p class="lead">An unlisted security is a financial instrument that is not traded on a formal
+                        exchange because it does not meet listing requirements. Unlisted securities are also called OTC
+                        securities, as trading is done on the over-the-counter
+                        (OTC) market mostly by market makers. Unlisted stocks can be tracked via pink sheets or on the
+                        OTCBB.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="row mt-5 mb-5">
+    <div class="row mt-5 mb-4">
         <div class="col-xl-3">
             <div class="card">
-                <img class="card-img-top img-fluid" src="images\hedge.jpg" alt="Card image cap">
+                <img class="card-img-top img-fluid" src="images\Hedge1.jpg" alt="Card image cap">
                 <div class="card-body p-3">
                     <blockquote class="blockquote mb-0">
                         <h4 class="card-title mt-0">HEDGING SERVICES </h4>
@@ -47,6 +75,19 @@
                             price movements in an asset
                         </p>
                     </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="col-9">
+            <div class="jumbotron m-2">
+                <div class="container">
+                    <h1 class="display-6"><b>Hedging Services<b></h1>
+                    <p class="lead">An unlisted security is a financial instrument that is not traded on a formal
+                        exchange because it does not meet listing requirements.
+                        Unlisted securities are also called OTC securities, as trading is done on the over-the-counter
+                        (OTC) market mostly by market makers.
+                        Unlisted stocks can be tracked via pink sheets or on the OTCBB.
+                    </p>
                 </div>
             </div>
         </div>
