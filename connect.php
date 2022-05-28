1799eacf83057a807d9f29466e8c37fedb435bd1
Syed Azhar <syed.azhar9465@gmail.com>
2022-05-28 10:59:25 +0530

    indentadion

--- connect.php
+++ connect.php
@@ -8,8 +8,8 @@
 
   //create connection
   $conn = mysqli_connect($servername, $username, $password, $dbname);
-  //check connection
   
+  //check connection
   if(!$conn) {
     die("Connection failed:".mysqli_connect_error());
   }
